77f8f9352f0a341226730f7ca8808f47cfba3a16
buongarzoni <christian.buongarzoni@etermax.com>
2023-05-13 12:21:54 -0300

    draft

--- app/src/main/java/com/ml/mati/MainActivity.kt
+++ app/src/main/java/com/ml/mati/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.ml.mati
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
+import android.graphics.ImageFormat
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Base64
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -24,17 +27,30 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.ml.custom.FirebaseCustomLocalModel
+import com.google.mlkit.common.model.LocalModel
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.custom.CustomImageLabelerOptions
 import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import com.ml.mati.ui.theme.MatiTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        println("CRB before local model")
+        val localModel = LocalModel.Builder()
+            .setAssetFilePath("cnn_small_quantized.tflite").build()
+        // or .setAbsoluteFilePath(absolute file path to model file)
+        // or .setUri(URI to model file)
+        println("CRB after local model")
+
         setContent {
             MatiTheme {
                 // A surface container using the 'background' color from the theme
@@ -42,7 +58,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GalleryButton()
+                    GalleryButton(localModel)
                 }
             }
         }
@@ -55,7 +71,9 @@ fun Greeting(name: String) {
 }
 
 @Composable
-private fun GalleryButton() = Box(
+private fun GalleryButton(
+    localModel: LocalModel,
+) = Box(
     modifier = Modifier.fillMaxSize(),
     contentAlignment = Alignment.BottomCenter,
 ) {
@@ -70,31 +88,55 @@ private fun GalleryButton() = Box(
                     string.value = "No photo"
                 } else {
                     if (Build.VERSION.SDK_INT < 28) {
-                        bitmap.value = MediaStore.Images.Media.getBitmap(context.contentResolver, it)
+                        val galleryBitmap = MediaStore.Images.Media.getBitmap(context.contentResolver, it)
+                        bitmap.value = galleryBitmap.scale2()
                     } else {
                         val source = ImageDecoder.createSource(context.contentResolver, it)
-                        bitmap.value = ImageDecoder.decodeBitmap(source)
+                        val galleryBitmap = ImageDecoder.decodeBitmap(source)
+                        bitmap.value = galleryBitmap.scale2()
                     }
                 }
             }
         }
     if (bitmap.value != null) {
-        val imageLabeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
-        val inputImage = InputImage.fromBitmap(bitmap.value!!, 0)
-        imageLabeler
-            .process(inputImage)
-            .addOnSuccessListener {
-                println("CRB success ")
-                val imageLabel = it.maxBy { imageLabel -> imageLabel.confidence }
-                it.forEach { imageLabels -> println("CRB image label: ${imageLabels.text},${imageLabels.confidence},${imageLabels.index}") }
+        println("CRB before image labeler options")
+        val customImageLabelerOptions = CustomImageLabelerOptions.Builder(localModel)
+            .setConfidenceThreshold(0.5f)
+            .setMaxResultCount(5)
+            .build()
+        println("CRB before labeler")
+        val imageLabeler = ImageLabeling.getClient(customImageLabelerOptions)
+        println("CRB after labeler")
+        //val imageLabeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
 
-                string.value = "The image is:${imageLabel.text} - confidence:${imageLabel.confidence}"
-                imageLabeler.close()
-            }
-            .addOnFailureListener {
-                println("CRB fail $it")
-                imageLabeler.close()
-            }
+        val inputImage = InputImage.fromBitmap(bitmap.value!!, 0)
+
+/*
+            val inputImage = InputImage.fromByteBuffer(
+                convertBitmapToByteBuffer(bitmap.value!!),
+                96,
+                96,
+                0,
+                ImageFormat.YV12)
+
+ */
+            println("CRB inputImage")
+            imageLabeler
+                .process(inputImage)
+                .addOnSuccessListener {
+                    println("CRB success ")
+                    val imageLabel = it.maxBy { imageLabel -> imageLabel.confidence }
+                    it.forEach { imageLabels -> println("CRB image label: ${imageLabels.text},${imageLabels.confidence},${imageLabels.index}") }
+
+                    string.value =
+                        "The image is:${imageLabel.text} - confidence:${imageLabel.confidence}"
+                    imageLabeler.close()
+                }
+                .addOnFailureListener {
+                    println("CRB fail $it")
+                    imageLabeler.close()
+                }
+
 
     }
     Column {
@@ -102,11 +144,42 @@ private fun GalleryButton() = Box(
         Button(
             onClick = {
                 galleryLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-                      },
+            },
         ) {
             Text(text = "Galería")
         }
     }
+}
+
+private fun Bitmap.scale() = Bitmap.createScaledBitmap(this, 96, 96, true)
+private fun Bitmap.scale2() = Bitmap.createBitmap(this, 0,0,96, 96)
+
+private fun convertBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
+    val byteBuffer = ByteBuffer.allocateDirect(Float.SIZE_BYTES*96*96*3)
+    byteBuffer.order(ByteOrder.nativeOrder())
+
+    val pixels = IntArray(96 * 96)
+    println("CRB antes del copy")
+    val newBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, false)
+    println("CRB despues del copy")
+    newBitmap.getPixels(pixels, 0, newBitmap.width, 0, 0, newBitmap.width, newBitmap.height)
+    println("CRB getPixels")
+    var pixel = 0
+    for (i in 0 until 96) {
+        for (j in 0 until 96) {
+            val pixelVal = pixels[pixel++]
+
+            byteBuffer.putFloat(((pixelVal shr 16 and 0xFF)) / 255.0f)
+            byteBuffer.putFloat(((pixelVal shr 8 and 0xFF) ) / 255.0f)
+            byteBuffer.putFloat(((pixelVal and 0xFF)) / 255.0f)
+
+        }
+    }
+    println("CRB recycle")
+    newBitmap.recycle()
+    println("CRB recycle 2")
+
+    return byteBuffer
 }
 
 @Preview(showBackground = true)
